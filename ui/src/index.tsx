47e0cdb1654a9082e43db0b20986082c3d6994dc
light23-i <74209656+light23-i@users.noreply.github.com>
2025-01-10 17:54:50 +0530

    Update index.tsx

--- ui/src/index.tsx
+++ ui/src/index.tsx
@@ -4,8 +4,8 @@ import './index.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import UserForm from './user.tsx';
 import App from './App5.tsx';
-import ThankYouPage from './tnx.jsx';
-import DetailedAssessmentPage from "./assp.jsx";
+import ThankYouPage from './tnx2.jsx';
+import DetailedAssessmentPage from "./assp2.jsx";
 ReactDOM.render(
   <React.StrictMode>
    <Router>
